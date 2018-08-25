082c441b3074ed9ed6de7f4840977455293422f2
Ryan Wiebe <wieberockin@yahoo.com>
2018-08-25 16:34:15 -0600

    Add project files.

---
+++ Decisions/Program.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decisions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bob's Big Giveaway");
+            Console.Write("Choose a door: 1, 2 or 3: ");
+            string userValue = Console.ReadLine();
+
+            if (userValue =="1")
+            {
+                string message = "You won a new car!";
+            }
+            else if (userValue == "2")
+            {
+                string message = "You won a boat!";
+            }
+            else if (userValue == "3")
+            {
+                string message = "You won a new cat!";
+            }
+            else
+            {
+                string message = "Sorry, we didn't understand";
+            }
+
+            Console.WriteLine(message);
+            Console.ReadLine();
+        }
+    }
+}
